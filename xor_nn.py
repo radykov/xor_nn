5aa6da68de953c6a87af247273fa5adcff67fe95
Rinat Sadykov <radykov@192-168-1-5.tpgi.com.au>
2017-02-11 16:05:20 +1100

    cleaned up code

--- xor_nn.py
+++ xor_nn.py
@@ -6,6 +6,7 @@ sess = tf.InteractiveSession()
 x_ = tf.placeholder(tf.float32, shape=[4,2], name="x-input")
 y_ = tf.placeholder(tf.float32, shape=[4,1], name="y-input")
 
+# Configure weights and layers
 W = tf.Variable(tf.random_uniform([2, 2], -.01, .01))
 b = tf.Variable(tf.random_uniform([2], -.01, .01))
 hidden  = tf.nn.relu(tf.matmul(x_,W) + b) # first layer.
@@ -13,18 +14,17 @@ hidden  = tf.nn.relu(tf.matmul(x_,W) + b) # first layer.
 W2 = tf.Variable(tf.random_uniform([2,1], -.1, .1))
 b2 = tf.Variable(tf.zeros([1]))
 hidden2 = tf.matmul(hidden, W2 + b2)
-
 y = tf.nn.softmax(hidden2)
+
+# Training function + data
 cross_entropy = -tf.reduce_sum(y_*tf.log(hidden2))
 train_step = tf.train.GradientDescentOptimizer(0.2).minimize(cross_entropy)
-# train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cost)
 
 XOR_X = [[0,0],[0,1],[1,0],[1,1]]
 XOR_Y = [[0],[1],[1],[0]]
 
 init = tf.global_variables_initializer()
 sess.run(init)
-
 # Train on the input data
 for i in range(100):
     sess.run([cross_entropy, train_step], feed_dict={x_: XOR_X, y_: XOR_Y})
